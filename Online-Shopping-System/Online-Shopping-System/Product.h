ccec9ca7e418dc0b8b881e21f6d71b799e2836f5
ahmed3baas <ahmed.3baas@gmail.com>
2020-10-31 14:03:54 +0200

    creat classes

---
+++ Online-Shopping-System/Online-Shopping-System/Product.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Product
+{
+private:
+	int productId;
+	double price;
+	string name, stutas;
+
+public:
+
+};
+
---
+++ Online-Shopping-System/Online-Shopping-System/StoreHouse.cpp
@@ -0,0 +1,1 @@
+#include "StoreHouse.h"
